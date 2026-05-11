cc604eb4a5a3abd335b9fbeffc1a5bf88183d638
Just Playin <ensminger@yahoo.com>
2026-05-11 11:46:36 -0400

    Adjust Docker image build time estimate
    Updated estimated time for building the Docker image.

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 clear
 echo "First we build the docker image"
-echo "This step takes 9-10 min to download and build the image(only the first time!)"
+echo "This step takes 3-10 min to download and build the image(only the first time!)"
 sleep 2
 sudo docker build -t kali .
 clear
